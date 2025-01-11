3e8a70084f21273417713592e56e0a16610d7a67
Arnaldo Cisneros <76781890+arnaldo10cisne@users.noreply.github.com>
2025-01-10 23:44:26 -0500

    Added MateriaShop_CICD_Stack to infrastructure.ts (#4)

--- infrastructure/bin/infrastructure.ts
+++ infrastructure/bin/infrastructure.ts
@@ -2,17 +2,29 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { MateriaShop_Serverless_Stack } from "../lib/serverless-stack";
+import { MateriaShop_CICD_Stack } from "../lib/cicd-stack";
 
 const app = new cdk.App();
-new MateriaShop_Serverless_Stack(app, "MateriaShop-ServerlessStack", {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
+const serverlessStack = new MateriaShop_Serverless_Stack(
+  app,
+  "MateriaShop-ServerlessStack",
+  {
+    /* If you don't specify 'env', this stack will be environment-agnostic.
+     * Account/Region-dependent features and context lookups will not work,
+     * but a single synthesized template can be deployed anywhere. */
+    /* Uncomment the next line to specialize this stack for the AWS Account
+     * and Region that are implied by the current CLI configuration. */
+    // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+    /* Uncomment the next line if you know exactly what Account and Region you
+     * want to deploy the stack to. */
+    env: { account: "706169966278", region: "us-east-1" },
+    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  },
+);
+
+new MateriaShop_CICD_Stack(app, "MateriaShop-CICDStack", {
   env: { account: "706169966278", region: "us-east-1" },
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  serverlessStackName: serverlessStack.serverlessStackName,
+  productsTableARN: serverlessStack.productsTableARN,
+  transactionsTableARN: serverlessStack.transactionsTableARN,
 });
