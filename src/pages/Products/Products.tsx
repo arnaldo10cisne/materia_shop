a341a8e07f888c13949adbce4bf0fc61ea42203d
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2025-01-12 13:41:51 -0500

    Created initial Products and userSelection pages

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { BlueBox } from "../../components/BlueBox/BlueBox.tsx";
+import { SelectableOption } from "../../components/SelectableOption/SelectableOption.tsx";
+import styles from "./Products.module.scss";
+import classNames from "classnames";
+import { useNavigate } from "react-router-dom";
 
 export const Products = () => {
-  return <>PRODUCTS LIST</>;
+  const navigate = useNavigate();
+
+  const handleClickReturn = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className={classNames(styles.Products)}>
+      <BlueBox>
+        <SelectableOption onClickHandler={handleClickReturn}>
+          Return
+        </SelectableOption>
+      </BlueBox>
+      <BlueBox>Product List</BlueBox>
+    </div>
+  );
 };
--- src/pages/UserSelection/UserSelection.tsx
+++ src/pages/UserSelection/UserSelection.tsx
@@ -1,5 +1,51 @@
 import React from "react";
+import { BlueBox } from "../../components/BlueBox/BlueBox.tsx";
+import { SelectableOption } from "../../components/SelectableOption/SelectableOption.tsx";
+import { CharacterPortrait } from "../../components/CharacterPortrait/CharacterPortrait.tsx";
+import styles from "./UserSelection.module.scss";
+import { UserModel } from "../../utils/models.ts";
+import { PLACEHOLDER_CHARACTER } from "../../utils/constants.ts";
+import classNames from "classnames";
+import { useNavigate } from "react-router-dom";
 
 export const UserSelection = () => {
-  return <>User SELECTION</>;
+  const navigate = useNavigate();
+
+  const characterList: UserModel[] = [
+    PLACEHOLDER_CHARACTER,
+    PLACEHOLDER_CHARACTER,
+    PLACEHOLDER_CHARACTER,
+    PLACEHOLDER_CHARACTER,
+    PLACEHOLDER_CHARACTER,
+    PLACEHOLDER_CHARACTER,
+    PLACEHOLDER_CHARACTER,
+    PLACEHOLDER_CHARACTER,
+    PLACEHOLDER_CHARACTER,
+    PLACEHOLDER_CHARACTER,
+  ];
+
+  const handleClickReturn = () => {
+    navigate("/");
+  };
+
+  const handleClickSelectCharacter = () => {};
+
+  return (
+    <div className={classNames(styles.UserSelection)}>
+      <BlueBox>
+        <SelectableOption onClickHandler={handleClickReturn}>
+          Return
+        </SelectableOption>
+      </BlueBox>
+      <BlueBox>
+        {characterList.map((character: UserModel) => {
+          return (
+            <SelectableOption onClickHandler={handleClickSelectCharacter}>
+              <CharacterPortrait character={character} showName={true} />
+            </SelectableOption>
+          );
+        })}
+      </BlueBox>
+    </div>
+  );
 };
