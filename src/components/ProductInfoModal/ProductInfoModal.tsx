a3fea45f720bcd086759ed384c9e42a394dabc60
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2025-01-12 22:53:50 -0500

    Add feature to add and remove products from cart

--- src/components/ProductInfoModal/ProductInfoModal.tsx
+++ src/components/ProductInfoModal/ProductInfoModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ProductInfoModal.module.scss";
 import classNames from "classnames";
 import { MateriaIconModel, ProductModel } from "../../utils/models.ts";
@@ -15,8 +15,21 @@ export const ProductInfoModal = ({
   product,
   onClose,
 }: ProductInfoModalProps) => {
+  const [amountInCart, setAmountInCart] = useState(0);
   const handleCloseButtonClick = () => {
     onClose();
+  };
+
+  const handleAddToCart = () => {
+    if (product && amountInCart < product.stock_amount) {
+      setAmountInCart(amountInCart + 1);
+    }
+  };
+
+  const handleRemoveFromCart = () => {
+    if (amountInCart > 0) {
+      setAmountInCart(amountInCart - 1);
+    }
   };
 
   if (!product) {
@@ -56,6 +69,23 @@ export const ProductInfoModal = ({
             <p className={classNames(styles.ProductDescription)}>
               {product.description}
             </p>
+
+            <p>Stock: {product.stock_amount}</p>
+            <div className={classNames(styles.AddToCartController)}>
+              <SelectableOption onClickHandler={handleRemoveFromCart}>
+                <span className={classNames(styles.ChangeCartAmount)}>-</span>
+              </SelectableOption>
+              <p className={classNames(styles.CartAmount)}>
+                Cart: {amountInCart}
+              </p>
+              <SelectableOption onClickHandler={handleAddToCart}>
+                <span className={classNames(styles.ChangeCartAmount)}>+</span>
+              </SelectableOption>
+            </div>
+
+            <SelectableOption onClickHandler={() => {}}>
+              Add to cart
+            </SelectableOption>
           </div>
         </BlueBox>
       </div>
