0230302f1524540fda1b1fc5ba2f01c3f106e4a1
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2025-01-13 11:23:44 -0500

    Created CartModal item list

--- src/components/CartModal/CartModal.tsx
+++ src/components/CartModal/CartModal.tsx
@@ -4,6 +4,11 @@ import classNames from "classnames";
 import { BlueBox } from "../BlueBox/BlueBox.tsx";
 import { SelectableOption } from "../SelectableOption/SelectableOption.tsx";
 import { playCancelCursorSfx } from "../../utils/utilityFunctions.ts";
+import { RootState } from "../../store/store.ts";
+import { useDispatch, useSelector } from "react-redux";
+import { MATERIA_LIST } from "../../utils/constants.ts";
+import { MateriaIconModel } from "../../utils/models.ts";
+import { removeCartItem, clearCartContent } from "../../store/cartReducer.ts";
 
 interface CartModalProps {
   onClose: () => any;
@@ -13,11 +18,14 @@ export const CartModal = ({ onClose }: CartModalProps) => {
   const handleCloseButtonClick = () => {
     onClose();
   };
+  const dispatch = useDispatch();
+
+  const cartContent = useSelector((state: RootState) => state.cart.currentCart);
 
   return (
     <div className={classNames(styles.CartModalContainer)}>
       <div className={classNames(styles.CartModal)}>
-        <BlueBox>
+        <BlueBox customStyles={styles.CartModalBlueBox}>
           <SelectableOption
             onClickHandler={handleCloseButtonClick}
             sfxOnClick={playCancelCursorSfx}
@@ -25,7 +33,78 @@ export const CartModal = ({ onClose }: CartModalProps) => {
           >
             Close
           </SelectableOption>
-          <p>CART CONTENT</p>
+          {cartContent.length === 0 ? (
+            <p className={classNames(styles.EmptyCartWarning)}>CART IS EMPTY</p>
+          ) : (
+            <>
+              {cartContent.map((cartItem) => {
+                return (
+                  <>
+                    <p className={classNames(styles.ProductName)}>
+                      <img
+                        className={classNames(styles.ProductIcon)}
+                        src={
+                          MATERIA_LIST.find((materia: MateriaIconModel) => {
+                            return (
+                              materia.type === cartItem.product.materia_type
+                            );
+                          })?.src
+                        }
+                        alt="materiaIcon"
+                      />
+                      {cartItem.product.name}
+                    </p>
+                    <div
+                      className={classNames(
+                        styles.CartItemInfoRow,
+                        styles.CartItemInfoLabelRow,
+                      )}
+                    >
+                      <p className={classNames(styles.CartItemInfoData)}>
+                        Unit Price * Units
+                      </p>
+                      <p className={classNames(styles.CartItemInfoData)}>
+                        Total Price
+                      </p>
+                    </div>
+                    <div className={classNames(styles.CartItemInfoRow)}>
+                      <p className={classNames(styles.CartItemInfoData)}>
+                        {cartItem.product.price} Gil * {cartItem.amount} units
+                      </p>
+                      <p className={classNames(styles.CartItemInfoData)}>
+                        {cartItem.total_price} Gil
+                      </p>
+                    </div>
+                    <SelectableOption
+                      onClickHandler={() => {
+                        dispatch(
+                          removeCartItem({
+                            product: cartItem.product,
+                            amount: cartItem.amount,
+                          }),
+                        );
+                      }}
+                      sfxOnClick={playCancelCursorSfx}
+                      customStyles={styles.RemoveItem}
+                    >
+                      <p className={classNames(styles.RemoveItem)}>
+                        Remove Item
+                      </p>
+                    </SelectableOption>
+                  </>
+                );
+              })}
+              <SelectableOption
+                onClickHandler={() => {
+                  dispatch(clearCartContent());
+                }}
+                sfxOnClick={playCancelCursorSfx}
+                customStyles={styles.EmptyCartOption}
+              >
+                <p className={classNames(styles.EmptyCart)}>Empty Cart</p>
+              </SelectableOption>
+            </>
+          )}
         </BlueBox>
       </div>
     </div>
