c6233d04e20bfb378a2105186a125451a3e74de4
Arnaldo Cisneros <arnaldo.jose.cisneros.zambrano@gmail.com>
2025-01-13 22:53:09 -0500

    Get users for dynamoDB now working

--- materia_shop_nestjs_app/src/users/users.service.ts
+++ materia_shop_nestjs_app/src/users/users.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { UserModel } from 'src/models';
+import {
+  DynamoDBClient,
+  ScanCommand,
+  GetItemCommand,
+} from '@aws-sdk/client-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
 
 @Injectable()
 export class UsersService {
-  findAllUsers() {
-    return [];
+  private readonly dynamoDBClient = new DynamoDBClient({ region: 'us-east-1' });
+  private readonly table_name = 'MateriaShop__users-table';
+
+  async findAllUsers() {
+    const command = new ScanCommand({ TableName: this.table_name });
+    const response = await this.dynamoDBClient.send(command);
+    return response.Items.map((item) => unmarshall(item));
   }
 
-  findOneUser(id: UserModel['id']) {
-    return { id };
+  async findOneUser(id: UserModel['id']) {
+    const command = new GetItemCommand({
+      TableName: this.table_name,
+      Key: marshall({ id }),
+    });
+    const response = await this.dynamoDBClient.send(command);
+    return response.Item ? unmarshall(response.Item) : null;
   }
 }
